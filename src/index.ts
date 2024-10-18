63548dea4ef73d846d93d363fe644a962b6c4e02
Matthaeus <matthaeusbm@gmail.com>
2024-10-18 01:28:00 -0300

    test deploy in render

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,16 @@
-// import express, { Application } from "express";
-// import dotenv from "dotenv";
-// import router from "./routes/index";
-// import cors from "cors";
-// dotenv.config();
+import express, { Application } from "express";
+import dotenv from "dotenv";
+import router from "./routes/index";
+import cors from "cors";
+dotenv.config();
 
-// const app: Application = express();
-// const port = process.env.PORT || 8000;
+const app: Application = express();
+const port = process.env.PORT || 8000;
 
-// app.use(express.json());
-// app.use(router);
-// app.use(cors());
+app.use(express.json());
+app.use(router);
+app.use(cors());
 
-// app.listen(port, () => {
-//   console.log(`Server running on port: ${port}`);
-// });
-
-const express = require("express");
-const app = express();
-
-app.get("/", (_req: any, res: any) => res.send("Express on Vercel"));
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
-module.exports = app;
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
